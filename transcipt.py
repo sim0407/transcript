f84ce97a87e3f91cd6573ec99c29dbd91f9e511c
sim0407 <63286897+sim0407@users.noreply.github.com>
2023-04-22 21:19:13 +0900

    Add ToDo

--- transcipt.py
+++ transcipt.py
@@ -1,9 +1,17 @@
 import whisper
 
+#TODO (必要であれば)入力ファイルの形式に合わせて変換を噛ませる
+#TODO GPUを使用するようにする
+#TODO printでなくtxtファイルとして出力する
+#TODO 出力ファイルを、元ファイルの名前に応じたものにする
+#TODO ファイル名をコード内でなく、プロンプトとして受け取る
+#TODO (未定)GUI化する
+
 def main():
+
     #tiny, base, small, medium, largeの5つのモデルが用意されている
     model = whisper.load_model("medium")
-    result = model.transcribe(r"audio_file.mp3")
+    result = model.transcribe(r"output.wav")
     print(result["text"])
 
 
