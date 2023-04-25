d81f71ced72a1c4e6e4d24cafd688c159164d6bf
sim0407 <63286897+sim0407@users.noreply.github.com>
2023-04-25 16:37:22 +0900

    Update transcript.py to output text file

--- transcipt.py
+++ transcipt.py
@@ -2,16 +2,16 @@ import whisper
 
 #TODO (必要であれば)入力ファイルの形式に合わせて変換を噛ませる
 #TODO GPUを使用するようにする
-#TODO printでなくtxtファイルとして出力する
-#TODO 出力ファイルを、元ファイルの名前に応じたものにする
 #TODO (未定)GUI化する
 
 def main():
     audio_file_name = input('ファイル名を入力してください').replace('\"', '')
+    txt_file_name = audio_file_name[:audio_file_name.find('.')] + '.txt'
     #tiny, base, small, medium, largeの5つのモデルが用意されている
     model = whisper.load_model("large")
     result = model.transcribe(audio_file_name)
     print(result["text"])
-    
+    with open(txt_file_name, mode='w') as f:
+        f.write(result["text"])
 
 main()
